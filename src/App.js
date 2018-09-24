0715c28e940a3b86090729106cf32b8d8cc36bf1
Luke Rollans <hello@lukerollans.me>
2018-09-24 18:47:21 +0800

    Introduce global styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Router, Link } from "react-static"
 import { hot } from "react-hot-loader"
 
 import Routes from "react-static-routes"
+import GlobalStyles from "./components/GlobalStyles"
 
 const App = () => (
   <Router>
-    <Routes />
+    <GlobalStyles>
+      <Routes />
+    </GlobalStyles>
   </Router>
 )
 
--- static.config.js
+++ static.config.js
@@ -1,19 +1,56 @@
+import React from "react"
+import { ServerStyleSheet } from "styled-components"
+
 export default {
   getSiteData: () => ({
-    title: 'Astronaut Lab',
+    title: "Astronaut Lab",
   }),
 
   // Routes and their respective data
   getRoutes: async () => {
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home",
       },
       {
         is404: true,
-        component: 'src/containers/404',
+        component: "src/containers/404",
       },
-    ];
+    ]
   },
-};
+
+  // We need to override `renderToHtml` and inject computed styles from
+  // styled-components
+  renderToHtml: (render, Comp, meta) => {
+    const sheet = new ServerStyleSheet()
+    const html = render(sheet.collectStyles(<Comp />))
+    meta.styleTags = sheet.getStyleElement()
+
+    return html
+  },
+
+  // We need to do a few custom things with the Document, such as pre-rendering
+  // styles and injecting some external stuff we need
+  Document: ({ Html, Head, Body, children, renderMeta }) => (
+    <Html>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        {/* Slab 27px - Google Fonts*/}
+        <link
+          href="https://fonts.googleapis.com/css?family=Slabo+27px"
+          rel="stylesheet"
+        />
+
+        {renderMeta.styleTags}
+
+        <title>Astronaut Lab</title>
+      </Head>
+    </Html>
+  ),
+}

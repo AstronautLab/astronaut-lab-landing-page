d640b7c8214a38d77133d0563abbab8b73dab627
Luke Rollans <hello@lukerollans.me>
2018-09-25 19:07:32 +0800

    Fix router page rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { Fragment } from "react"
+import { Router } from "react-static"
 import Routes from "react-static-routes"
 import GlobalStyles from "./components/GlobalStyles"
 
 import { hot } from "react-hot-loader"
 
 const App = () => (
-  <GlobalStyles>
-    <Routes />
-  </GlobalStyles>
+  <Fragment>
+    <GlobalStyles />
+
+    <Router>
+      <Routes />
+    </Router>
+  </Fragment>
 )
 
 export default hot(module)(App)

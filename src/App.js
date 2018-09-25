9e8c41210ea7fb48d6f4c7c4e8364a524cf848c1
Luke Rollans <hello@lukerollans.me>
2018-09-25 19:01:12 +0800

    Strip out some unused stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react"
-import { Router, Link } from "react-static"
-import { hot } from "react-hot-loader"
-
 import Routes from "react-static-routes"
 import GlobalStyles from "./components/GlobalStyles"
 
+import { hot } from "react-hot-loader"
+
 const App = () => (
   <GlobalStyles>
-    <Router>
-      <Routes />
-    </Router>
+    <Routes />
   </GlobalStyles>
 )
 
